2c0af7dfd6b84524d9209cb3495ae47d05785fb8
ChernyshDaryna <darina.chernysh@krfk.kai.edu.ua>
2025-02-13 12:27:38 +0200

    lab2

---
+++ lab2/lab2.js
@@ -0,0 +1,34 @@
+// 1
+var n1 = 1552;
+let n2= "Hi";
+const n3 = 5;
+// 2
+console.log(typeof n1);
+console.log(typeof n2); 
+console.log(typeof n3); 
+// 3
+let student = {
+    name: "Daryna",
+    age: 17,
+    st: true
+};
+//4
+student.university = "koledzh";
+console.log(student);
+//5
+n2 = 5;
+console.log(typeof n2); 
+
+/* Оголоси змінну message та запиши в неї повідомлення про покупку,
+рядок у форматі: "You picked <имя товара>, price per item is <цена товара> credits".
+ Де <имя товара> це <цена товара> значення змінних productName і pricePerItem.
+  Використовуй синтаксис шаблонних рядків. 
+
+  const productName = "Droid";
+  const pricePerItem = 3500;
+  
+
+const greeting = `You picked ${productName}, price per item is  ${pricePerItem} credits!`;
+console.log(greeting); // "Welcome Mango, your room number is 207!"
+*/
+  // Change code below this line
